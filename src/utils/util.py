405f6ba5c9788ed0437c2dd540cf527a9a5e9a68
masroora1 <AidinBnn378@gmail.com>
2025-09-20 11:10:31 +0000

    adding bg box.

--- src/utils/util.py
+++ src/utils/util.py
@@ -21,10 +21,11 @@ class ImageExtractor():
     def extract_bboxed_images(self,imgs,bboxes,max_objects_in_scene)-> torch.tensor:
         assert imgs.dim()==3 # imgs.shape=[C,H,W]
 
-        output=torch.ones(size=(max_objects_in_scene,*imgs.shape),device=imgs.device) # shape=[max_objects_in_scene, C, H, W]
+        output=torch.zeros(size=(max_objects_in_scene,*imgs.shape),device=imgs.device) # shape=[max_objects_in_scene, C, H, W]
+        output[0]=imgs
         for idx,box in enumerate(bboxes):
             if torch.all(box == -1):
                 continue
             output[idx+1,:,int(box[1].item()):int(box[3].item()),int(box[0].item()):int(box[2].item())]=imgs[:,int(box[1].item()):int(box[3].item()),int(box[0].item()):int(box[2].item())]
-            #output[0]=imgs-imgs[:,int(box[1].item()):int(box[3].item()),int(box[0].item()):int(box[2].item())]
+            output[0,:,int(box[1].item()):int(box[3].item()),int(box[0].item()):int(box[2].item())]=0
         return output
