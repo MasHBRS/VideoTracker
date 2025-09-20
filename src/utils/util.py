aab9a95725cec64057b655cb60a8f1c55ec92b3e
masroora1 <AidinBnn378@gmail.com>
2025-09-20 08:12:18 +0000

    extract_masked_images

--- src/utils/util.py
+++ src/utils/util.py
@@ -2,10 +2,18 @@
 TODO:
 1. add background extract as well to the output 
 """
+import torch
+
+
 class ImageExtractor():
     def __init__(self) -> None:
         pass
-    def extract_masked_images(self)-> None: 
-        pass
-    def extract_bboxed_images(self)-> None:
+    def extract_masked_images(self,imgs,masks,max_objects_in_scene)-> torch.Tensor:
+        assert imgs.dim()==3 # imgs.shape=[C,H,W]
+
+        output=torch.zeros(size=(max_objects_in_scene,*imgs.shape),device=imgs.device) # shape=[mask_number, C, H, W]
+        for uc in masks.unique():
+            output[uc.item()]=imgs*(masks==uc)
+        return output 
+    def extract_bboxed_images(self,imgs,boxed_imgs)-> torch.tensor:
         pass
