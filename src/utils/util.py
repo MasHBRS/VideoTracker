5ccc5d6068b09f3c5cda090652397611ccd811e4
masroora1 <AidinBnn378@gmail.com>
2025-09-20 06:24:39 +0000

    adding util.

---
+++ src/utils/util.py
@@ -0,0 +1,11 @@
+"""
+TODO:
+1. add background extract as well to the output 
+"""
+class ImageExtractor():
+    def __init__(self) -> None:
+        pass
+    def extract_masked_images(self)-> None: 
+        pass
+    def extract_bboxed_images(self)-> None:
+        pass
