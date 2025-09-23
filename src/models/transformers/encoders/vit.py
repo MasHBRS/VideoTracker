40d1a27213ea94eaddacaafd73e5ad099c9700ad
masroora1 <AidinBnn378@gmail.com>
2025-09-23 13:48:24 +0000

    adapting vit to our problem.

--- src/models/transformers/encoders/vit.py
+++ src/models/transformers/encoders/vit.py
@@ -22,19 +22,20 @@ class ViT(nn.Module):
     Vision Transformer for image classification
     """
 
-    def __init__(self, patch_size, token_dim, attn_dim, num_heads, mlp_size, num_tf_layers, num_classes):
+    def __init__(self, img_height,img_width,channels,frame_numbers, token_dim, attn_dim, num_heads, mlp_size, num_tf_layers, num_classes,):
         """ Model initializer
          num_tf_layers : The number of transformer blocks that we need
            """
         super().__init__()
 
-        # breaking image into patches, and projection to transformer token dimension
-        self.pathchifier = Patchifier(patch_size)
+        """# breaking image into patches, and projection to transformer token dimension
+        self.pathchifier = Patchifier(patch_size)"""
 
         ''' Creating the embedding for each image patch/token'''
+        
         self.patch_projection = nn.Sequential(   
-                nn.LayerNorm(patch_size * patch_size * 3),
-                nn.Linear(patch_size * patch_size * 3, token_dim) # token_dim = token embedding
+                nn.LayerNorm(img_height * img_width * channels),
+                nn.Linear(img_height * img_width * channels, token_dim) # token_dim = token embedding
             )
 
         # adding CLS token and positional embedding
@@ -43,7 +44,7 @@ class ViT(nn.Module):
         is updated through training with GD. 
         / (token_dim ** 0.5): a common trick to stabilize training by keeping the scale of weights roughly controlled (similar to Xavier initialization)'''
         self.cls_token = nn.Parameter(torch.randn(1, token_dim) / (token_dim ** 0.5), requires_grad=True)
-        self.pos_emb = PositionalEncoding(token_dim) # return token embeddings + positional encoding
+        self.pos_emb = PositionalEncoding(token_dim,max_len=frame_numbers) # return token embeddings + positional encoding
 
         # cascade of transformer blocks
         transformer_blocks = [
@@ -72,7 +73,7 @@ class ViT(nn.Module):
                     output.append(self.extractor.extract_masked_images(x[batchIndx,frameIndx],masks=apply_mask[batchIndx,frameIndx],max_objects_in_scene=max_objects_in_scene)[0])
                 elif apply_bbox is not None:
                     output.append(self.extractor.extract_bboxed_images(x[batchIndx,frameIndx],bboxes=apply_bbox[batchIndx,frameIndx],max_objects_in_scene=max_objects_in_scene))
-        outputTorch=torch.stack(output,dim=0).reshape(B,frame_numbers,max_objects_in_scene,channels,height,width)
+        outputTorch=torch.stack(output,dim=0).to(x.device).reshape(B,frame_numbers,max_objects_in_scene,channels,height,width)
         return outputTorch
 
     def forward(self, x, boxes=None,masks=None,max_objects_in_scene=0): # full Transformer encoder block forward pass
@@ -81,27 +82,37 @@ class ViT(nn.Module):
         """
         assert (boxes==None) ^ (masks==None), "One of boxes or masks should be valued, and not both!" # either one of them should be non-empty.
         assert x.dim()==5
+        if (x.device!=next(self.parameters()).device):
+            x=x.to(next(self.parameters()).device)
+        if (masks is not None) and masks.device!=x.device:
+            masks=masks.to(x.device)
+        if (boxes is not None) and boxes.device!=x.device:
+            boxes=boxes.to(x.device)
         B,frame_numbers,channels,height,width = x.shape
         
-
-        self.apply_masks_or_bboxes(x,apply_mask=masks,apply_bbox=boxes,max_objects_in_scene=max_objects_in_scene)
-        # breaking image into patches, and projection to transformer token dimension
+        #(B,frame_numbers,max_objects_in_scene,channels,height,width)
+        filtered_imgs=self.apply_masks_or_bboxes(x,apply_mask=masks,apply_bbox=boxes,max_objects_in_scene=max_objects_in_scene) 
+        filtered_imgs=filtered_imgs.reshape(B,frame_numbers,max_objects_in_scene,-1)#(B,frame_numbers,max_objects_in_scene, channels * height * width)
+        
+        """# breaking image into patches, and projection to transformer token dimension
         patches = self.pathchifier(x)  # (B, 16, 8 * 8 * 3)
-        patch_tokens = self.patch_projection(patches)  # (B, 16, D)
+        """
+        patch_tokens = self.patch_projection(filtered_imgs)  # (B,frame_numbers,max_objects_in_scene, token_dim)
 
-        # concatenating CLS token and adding positional embeddings
+        """# concatenating CLS token and adding positional embeddings
         cur_cls_token = self.cls_token.unsqueeze(0).repeat(B, 1, 1) # shape: (B, 1, D).  B copies of the token, one for each sample in the batch.
         tokens = torch.cat([cur_cls_token, patch_tokens], dim=1)  # ~(B, 1 + 16, D) So now, each input sequence starts with the [CLS] token, followed by the patch tokens.
-        # ---> one token for all the patches of one image, not per batch. Summary token of all image
-        tokens_with_pe = self.pos_emb(tokens)
+        # ---> one token for all the patches of one image, not per batch. Summary token of all image"""
+        
+        tokens_with_pe = self.pos_emb(patch_tokens)
 
         # processing with transformer
         out_tokens = self.transformer_blocks(tokens_with_pe) # shapes?
-        out_cls_token = out_tokens[:, 0]  # fetching only CLS token
+        #out_cls_token = out_tokens[:, 0]  # fetching only CLS token
 
         # classification
-        logits = self.classifier(out_cls_token)
-        return logits
+        #logits = self.classifier(out_cls_token)
+        return out_tokens
 
 
     def get_attn_mask(self):
