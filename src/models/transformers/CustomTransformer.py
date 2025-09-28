9715900b520e85f2398b9ff4f3658ce2cde3fe89
masroora1 <AidinBnn378@gmail.com>
2025-09-28 11:06:02 +0000

    normalizing the encoder,decoder output.

--- src/models/transformers/CustomTransformer.py
+++ src/models/transformers/CustomTransformer.py
@@ -24,5 +24,10 @@ class CustomizableTransformer(nn.Module):
         self.decoder=decoder
     def forward(self, x, boxes=None,masks=None):
         encoded=self.encoder(x, boxes,masks)
-        decoded=self.decoder(encoded.unsqueeze(2))
+        normalized=self.normalize(encoded)
+        decoded=self.normalize(self.decoder(normalized.unsqueeze(2)))
         return decoded
+    def normalize(self,x):
+        min,max=x.min(),x.max()
+        norm=(x-min)/(max-min+1e-8)
+        return norm
