402ed24725a02857ee247004b4d04fef28a7e20f
masroora1 <AidinBnn378@gmail.com>
2025-09-28 11:00:16 +0000

    seems a good loss func.

--- src/utils/loss_function.py
+++ src/utils/loss_function.py
@@ -1,18 +1,19 @@
 import torch
 import torch.nn as nn
-#import lpips
+#from lpips  import  LPIPS
 from pytorch_msssim import SSIM, MS_SSIM  # You can also use 'torchmetrics.functional.structural_similarity_index_measure'
-
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from utils.util import device
 class ReconstructionLoss_L1_Ssim(nn.Module):
     def __init__(self, device='cpu', lambda_l1=1.0, lambda_ssim=0.5, lambda_lpips=0.1):
         super().__init__()
         self.lambda_l1 = lambda_l1
         self.lambda_ssim = lambda_ssim
         self.lambda_lpips = lambda_lpips
-
+        self.device=device
         self.ssim = SSIM(data_range=1.0, size_average=True, channel=3)  # For RGB
         self.l1 = nn.L1Loss()
-        #self.lpips_loss = lpips.LPIPS(net='vgg').to(device)  # LPIPS uses [-1,1] range
+        self.lpips_loss = LearnedPerceptualImagePatchSimilarity(net_type='alex').to(device)  # LPIPS uses [-1,1] range
 
     def forward(self, recon, target):
         # Ensure images are in [0,1] for SSIM
@@ -22,13 +23,23 @@ class ReconstructionLoss_L1_Ssim(nn.Module):
 
         # Compute SSIM loss
         ssim_loss = 1 - self.ssim(recon_flat, target_flat)
-
+        recon_flat=target_flat=None
         # L1 loss
         l1_loss = self.l1(recon, target)
 
         # LPIPS expects [-1,1]
-        #lpips_loss = self.lpips_loss(recon_flat, target_flat).mean()
+        lpips_loss = self.compute_loss_metric(recon, target,self.lpips_loss)
 
         # Weighted sum
-        loss = self.lambda_l1 * l1_loss + self.lambda_ssim * ssim_loss #+ self.lambda_lpips * lpips_loss
+        loss = self.lambda_l1 * l1_loss + self.lambda_ssim * ssim_loss + self.lambda_lpips * lpips_loss
         return loss
+    
+    def compute_loss_metric(self,tensor1, tensor2,metric):
+        batch_size, T, C, H, W = tensor1.shape
+        losses = []
+        for i in range(0, batch_size, 1):  # One batch at a time
+            for t in range(T): # One time step at a time
+                loss = metric(tensor1[i:i+1, t, :, :, :], tensor2[i:i+1, t, :, :, :])
+                losses.append(loss.item())
+        torch.cuda.empty_cache()  # Clear memory
+        return sum(losses)/len(losses)
