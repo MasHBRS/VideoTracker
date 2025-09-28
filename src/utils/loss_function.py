9bc901df4cf8c36b166dca4a27f95f633fd1d523
Aidin <Aidin_cuda1@github.com>
2025-09-28 00:16:55 +0000

    loss_function l1, ssim

--- src/utils/loss_function.py
+++ src/utils/loss_function.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
-import lpips
+#import lpips
 from pytorch_msssim import SSIM, MS_SSIM  # You can also use 'torchmetrics.functional.structural_similarity_index_measure'
 
-class ReconstructionLoss_L1_Ssim_Lpips(nn.Module):
+class ReconstructionLoss_L1_Ssim(nn.Module):
     def __init__(self, device='cpu', lambda_l1=1.0, lambda_ssim=0.5, lambda_lpips=0.1):
         super().__init__()
         self.lambda_l1 = lambda_l1
@@ -12,7 +12,7 @@ class ReconstructionLoss_L1_Ssim_Lpips(nn.Module):
 
         self.ssim = SSIM(data_range=1.0, size_average=True, channel=3)  # For RGB
         self.l1 = nn.L1Loss()
-        self.lpips_loss = lpips.LPIPS(net='vgg').to(device)  # LPIPS uses [-1,1] range
+        #self.lpips_loss = lpips.LPIPS(net='vgg').to(device)  # LPIPS uses [-1,1] range
 
     def forward(self, recon, target):
         # Ensure images are in [0,1] for SSIM
@@ -27,8 +27,8 @@ class ReconstructionLoss_L1_Ssim_Lpips(nn.Module):
         l1_loss = self.l1(recon, target)
 
         # LPIPS expects [-1,1]
-        lpips_loss = self.lpips_loss(recon_flat, target_flat).mean()
+        #lpips_loss = self.lpips_loss(recon_flat, target_flat).mean()
 
         # Weighted sum
-        loss = self.lambda_l1 * l1_loss + self.lambda_ssim * ssim_loss + self.lambda_lpips * lpips_loss
+        loss = self.lambda_l1 * l1_loss + self.lambda_ssim * ssim_loss #+ self.lambda_lpips * lpips_loss
         return loss
